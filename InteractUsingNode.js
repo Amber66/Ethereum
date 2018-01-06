bb641b905ba846430be4ba8a86877986a8176245
Amber <zhuxing.li96@gmail.com>
2018-01-07 10:06:31 +1300

    Update InteractUsingNode.js

--- InteractUsingNode.js
+++ InteractUsingNode.js
@@ -1,10 +1,8 @@
------------
-
 var Web3 = require('web3');
 var web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"));
 eth=web3.eth;
-
+// way 1:
 src= "" +
 "pragma solidity ^0.4.6;\n"  +
 "contract myTestContract {\n" +
@@ -13,12 +11,17 @@ src= "" +
 "   }\n" +
 "}\n";
 
-inputName="\'localContract\'"
-inputContractName='myTestContract'
+//Way 2:
+// input ={
+//     'inputName' : src,
+//     'fileContract' : fs.readFileSync('/tmp/porosity/vulnerable.sol', 'utf8'),
+// }
+
+//attention: need to change file path
 input ={
-    'fileContract' : fs.readFileSync('/tmp/porosity/vulnerable.sol', 'utf8'),
-    'inputName' : src,
-}
+    'fileContract' : fs.readFileSync('/Users/amber/projects/contracts/nfcEscrow.sol', 'utf8'),
+}//tmp/nfcEscrow.sol
+
 solc=require('solc')
 
 
@@ -27,8 +30,10 @@ solc=require('solc')
 // abi = compiled.info.abiDefinition;
 
 compiled=solc.compile({sources:input}, 1)
-abi=JSON.parse(compiled.contracts['inputName:myTestContract'].interface)
-bytecode =  compiled.contracts['inputName:myTestContract'].bytecode
+
+//attention: ensure the key is correct. format: input:Contract name
+abi=JSON.parse(compiled.contracts['fileContract:nfcEscrow'].interface)
+bytecode =  compiled.contracts['fileContract:nfcEscrow'].bytecode
 
 gasEstimate = web3.eth.estimateGas({data: bytecode});
 
@@ -45,4 +50,5 @@ eth.contract(abi).new(
     console.log('address: ' + contract.address);
   }
 });
+
 inst.multiply(2, 15)
